cf0afa7c78eb8d76e58a878d5451e87b654e011b
vinaysakala <vinaysakala2002@gmail.com>
2026-01-03 11:43:49 +0530

    03-01-2025

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.vinay.hotelapp',
   appName: 'hotelapp',
   webDir: 'www'
 };
