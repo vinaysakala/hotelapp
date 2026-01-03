6c400603455de33fb7e907783288483152c930d2
vinaysakala <vinaysakala2002@gmail.com>
2026-01-03 18:40:01 +0530

    app icon and splash screens created and apk built

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,17 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.vinay.hotelapp',
   appName: 'hotelapp',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000, // Show for 2 seconds
+      backgroundColor: "#ffffff",
+      showSpinner: false,
+      androidScaleType: "CENTER_CROP",
+      splashFullScreen: true,
+      splashImmersive: true,
+    },
+  },
 };
 
 export default config;
